470fb505e16f8d680ba5a52ff80165f458531261
Arjun <arjun40779@gmail.com>
2023-05-23 23:29:23 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const p = document.createElement("p");
+const data = fetch("https://api.kanye.rest/")
+  .then((response) => response.json())
+  .then((data) => (p.innerText = data.quote + "\n- Kanye West"));
+
+p.innerText = data.quote;
+console.log(data);
+document.querySelector(".hero-text").append(p);
+
 let nav = document.getElementById("navlist");
 const mobnav = document.getElementById("mobnav");
 mobnav.addEventListener("click", toggle);
