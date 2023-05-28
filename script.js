850434a387e96d95109ad9b317d6c20bb0d6a15e
Arjun <arjun40779@gmail.com>
2023-05-28 22:41:11 +0530

    ipdate

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ const data = fetch("https://api.kanye.rest/")
   .then((data) => (p.innerText = data.quote + "\n- Kanye West"));
 
 /* _________________________________________________ */
-// navigation toggle
+// navigation
 document.querySelector(".hero-text").append(p);
 
 let nav = document.getElementById("navlist");
 const mobnav = document.getElementById("mobnav");
 mobnav.addEventListener("click", toggle);
 function toggle() {
-  if (nav.style.display == "none") {
+  console.log(nav.style.display);
+  if (nav.style.display != "flex") {
     nav.style.display = "flex";
   } else {
     document.getElementById("navlist").style.display = "none";
